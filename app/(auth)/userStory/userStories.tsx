7c2ee07936a64c55ce4734f8f291f8055d86f337
unknown <md.tazwarul.islam.07@gmail.com>
2025-03-02 13:19:58 +0600

    update

--- app/(auth)/userStory/userStories.tsx
+++ app/(auth)/userStory/userStories.tsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/lib/ThemeProvider/ThemeProvider";
 import React from "react";
+import { View } from "react-native";
+import * as Progress from "react-native-progress";
 
-const userStories = () => {
+const UserStories = () => {
   return (
     <View>
-      <Text>userStories</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Progress.Bar
+          borderRadius={10}
+          color={theme.primaryTextColor}
+          style={{ flex: 1, marginRight: 5 }}
+          borderWidth={0.5}
+          progress={0.5}
+          height={5}
+          width={null}
+          animationType="spring"
+          animated
+        />
+        <Progress.Bar
+          animationType="spring"
+          animated
+          borderRadius={10}
+          color={theme.primaryTextColor}
+          width={null}
+          style={{ flex: 1 }}
+          borderWidth={0.5}
+          progress={0.5}
+          height={5}
+        />
+      </View>
     </View>
   );
 };
 
-export default userStories;
-
-const styles = StyleSheet.create({});
+export default UserStories;
